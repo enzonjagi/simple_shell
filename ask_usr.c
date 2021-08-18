827cf5bb36f1b51398fe989baf017a8b506b2ae2
enzonjagi <amonsdungo@gmail.com>
2021-08-18 11:02:35 +0300

    Shell skeleton update

--- ask_usr.c
+++ ask_usr.c
@@ -10,19 +10,24 @@ int main(void)
 {
 	char *usrIn;/*Input*/
 	size_t size = 100;/*Expected size of input*/
-	int t = 4;/*Number of lines of input*/
+	int t = 1, get;/*Number of lines of input*/
 	char **string_ptr = NULL;/*Output*/
 
 	usrIn = (char *) malloc(size);
 	if (usrIn == NULL)
 		return (free(usrIn), -1);
 	string_ptr = &usrIn;
-	while (t--) /*Use this to check for EOF to break the loop*/
+	while (t)
 	{
 		printf("$ ");
-		getline(string_ptr, &size, stdin);
+		get = getline(string_ptr, &size, stdin);
+		if (get == -1)/*check for EOF*/
+			t = 0;/*change flag to 0, hence end loop*/
 		while (usrIn[0] == '\n')
+		{
+			printf("$ ");
 			getline(string_ptr, &size, stdin);
+		}
 		printf("%s", usrIn);
 	}
 	free(usrIn);
